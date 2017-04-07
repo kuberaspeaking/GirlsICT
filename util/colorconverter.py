43e78d88ed7b8acfb0a355c56746ba387ffefd68
a3zzat <viva.amego@gmail.com>
2017-04-07 18:15:39 +0200

    modify python script.

--- util/colorconverter.py
+++ util/colorconverter.py
@@ -19,12 +19,41 @@ colorlist = [(0,0,0),
              (0,128,128),
              (0,0,128)]
 
+NamesList=[
+'off    ',
+'White  ',
+'Red    ',
+'Lime   ',
+'Blue   ',
+'Yellow ',
+'Cyan   ',
+'Magenta',
+'Silver ',
+'Gray   ',
+'Maroon ',
+'Olive  ',
+'Green  ',
+'Purple ',
+'Teal   ',
+'Navy   '
+]		 
+			 
 def convertColor(colorTuple):
-    r = 1023 - 4*colorTuple[0]
-    g = 1023 - 4*colorTuple[1]
-    b = 1023 - 4*colorTuple[2]
-    return (r,g,b)
+    r = 255 - colorTuple[0]
+    g = 255 - colorTuple[1]
+    b = 255 - colorTuple[2]
+    return str((r,g,b))
 
 convertedColorList = [convertColor(color) for color in colorlist]
-
-print(str(convertedColorList).replace('(', '{').replace(')', '}'))
+ColorVals =[]
+for i in range(len(convertedColorList)):
+	spaces = ' '
+	ColorVal = convertedColorList[i].replace('(', '{').replace(')', '}')+ ','
+	spaces = spaces *(len(convertedColorList[0])-len(convertedColorList[i]))
+	ColorVal += spaces
+	ColorVals.append(ColorVal)
+assert len(ColorVals) == len(NamesList)
+for i in range(len(ColorVal)):
+	FinalList =[(ColorVals[i]+ ' //'+ NamesList[i]) for i in range(len(ColorVals))]
+for final in FinalList:
+	print(final)
